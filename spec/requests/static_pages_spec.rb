cd9444b4d8ff7bf8c103ed70e0f2f668adae74b0
Gabriel Fuentes <gffuentes@gmail.com>
2014-07-07 00:13:28 -0400

    finish layout and routes

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,38 +1,39 @@
 require 'spec_helper'
 
 describe "Static pages" do
+  
+  subject { page }
+
   describe "Home page" do
+    before { visit root_path }
 
-  	it "should have the content 'Sample App' " do
-  		visit '/static_pages/home'
-  		expect(page).to have_content('Sample App')
-    end
+    	it { should have_selector('h1', text: 'Sample App' ) }
+      it { should have_title( full_title('') ) }
     
-    it "should have title 'Home'" do
-    	visit '/static_pages/home'
-    	expect(page).to have_title('Ruby on Rails Tutorial Sample App | Home')
-	end
   end
+
   describe "Help page" do
-  	it "should have the content 'Help'" do
-  		visit '/static_pages/help'
-  		expect(page).to have_content('Help')
-  	end
+    before { visit help_path }
 
-  	it "should have title 'Help'" do
-    	visit '/static_pages/help'
-    	expect(page).to have_title('Ruby on Rails Tutorial Sample App | Help')
-	end
+    	it { should have_selector('h1', text: 'Help' ) }
+      it { should have_title( full_title('Help') ) }
+
   end
+
   describe "About page" do
-  	it "should have the content 'About Us' " do
-  		visit '/static_pages/about'
-  		expect(page).to have_content('About Us')
-  	end
+  	before { visit about_path }
 
-  	it "should have title 'About'" do
-    	visit '/static_pages/about'
-    	expect(page).to have_title('Ruby on Rails Tutorial Sample App | About')
-	end
+      it { should have_selector('h1', text: 'About' ) }
+      it { should have_title( full_title('About') ) }
+
   end
+
+  describe "Contact page" do
+    before { visit contact_path }
+
+      it { should have_selector('h1', text: 'Contact' ) }
+      it { should have_title( full_title('Contact') ) }
+
+  end
+
 end
